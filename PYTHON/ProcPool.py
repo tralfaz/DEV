53519f3f648f3f71c0c0f2258537b5459a361eed
Michael Marchionna <michael.marchionna@gmail.com>
2021-04-27 12:54:29 -0700

    Create ProcPool.py

---
+++ PYTHON/ProcPool.py
@@ -0,0 +1,30 @@
+import concurrent.futures
+import math
+
+PRIMES = [
+    112272535095293,
+    112582705942171,
+    112272535095293,
+    115280095190773,
+    115797848077099,
+    1099726899285419]
+
+def is_prime(n):
+    if n % 2 == 0:
+        return False
+
+    sqrt_n = int(math.floor(math.sqrt(n)))
+    for i in range(3, sqrt_n + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+def main():
+    nprime = 1
+    with concurrent.futures.ProcessPoolExecutor(max_workers=3) as executor:
+        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
+            print('[%s] %d is prime: %s' % (nprime, number, prime))
+            nprime += 1
+
+if __name__ == '__main__':
+    main()
